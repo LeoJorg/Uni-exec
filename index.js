401a8ffe5d8b4dbe30d37a6e047e3b4db134a1a1
LeoJorg <leojorge.ofc.dev@gmail.com>
2022-03-12 18:28:51 -0300

    implementando o index.js.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!")
+const { exec } = require('shelljs');
+console.log('{ Executor de codigo Universal }');
+
+const request = {
+    content: `print("quanto é 7 + 7?")`,
+    lenguage: 'python'
+}
+
+function command(request) {
+    if (request.lenguage == 'js') {
+        return exec(`node -p '${request.content}'`, {silent:true}).stdout;
+    }
+    if (request.lenguage == 'python') {
+        return exec(`python -c '${request.content}'`, {silent:true}).stdout;
+    }
+}
+
+const response = command(request);
+console.log(response);
